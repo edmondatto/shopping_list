a44e9f42a7e370f1c75e6d6bf7bace981ba53a87
Edmond B. Atto <edmond@Edmonds-MacBook.local>
2018-01-12 17:14:10 +0300

    Create header component

---
+++ src/components/header.js
@@ -0,0 +1,7 @@
+import React, {Component} from 'react';
+
+export const Header = () => (
+  <nav className="navbar navbar-inverse bg-primary">
+    <a className="navbar-brand" id="app-logo">Shopping List App</a>
+  </nav>
+);
